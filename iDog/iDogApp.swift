51db0a8e5b1f05bcd644d3dc4ecc5bcb60756afa
Kevin Malkic <k_malkic@yahoo.fr>
2024-09-03 14:48:09 +0100

    Project creation

---
+++ iDog/iDogApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDogApp.swift
+//  iDog
+//
+//  Created by Kevin Malkic on 03/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct iDogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iDogUITests/iDogUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  iDogUITestsLaunchTests.swift
+//  iDogUITests
+//
+//  Created by Kevin Malkic on 03/09/2024.
+//
+
+import XCTest
+
+final class iDogUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
